c6c00f1113ee0edfd2f6d5c9ac102df1523e0ebe
ferdous-al-quraeshi <ecopavel@gmail.com>
2023-05-30 11:36:34 +0600

    assignment-2: done and submitted in pdf, md & py

---
+++ Assignment-2/assignment_2_submission.py
@@ -0,0 +1,12 @@
+# Assignment-2:
+    # Sum all the odd numbers from 0 to 100 and print it to the screen.
+
+
+# Code:
+sum_of_odds = 0
+
+for n in range(100):
+    if n % 2 != 0: # filter odds
+        sum += n
+
+print("The sum of odd numbers from 0 to 100 is:", sum_of_odds)
